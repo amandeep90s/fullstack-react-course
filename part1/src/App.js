e177df9d49a073e762785224a805239454069428
Amandeep Singh <programmeramandeep@gmail.com>
2023-01-02 21:06:43 +0530

    Unicafe project added

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [clicks, setClicks] = useState({
+		left: 0,
+		right: 0,
+	});
+	const [allClicks, setAllClicks] = useState([]);
+
+	const handleLeftClick = () => {
+		setAllClicks(allClicks.concat('L'));
+		setClicks({ ...clicks, left: clicks.left + 1 });
+	};
+
+	const handleRightClick = () => {
+		setAllClicks(allClicks.concat('R'));
+		setClicks({ ...clicks, right: clicks.right + 1 });
+	};
+
+	return (
+		<>
+			{clicks.left}
+			<Button handleClick={handleLeftClick} text='Left' />
+			<Button handleClick={handleRightClick} text='Right' />
+			{clicks.right}
+			<History allClicks={allClicks} />
+		</>
+	);
+};
+
+const Button = ({ handleClick, text }) => (
+	<button onClick={handleClick}>{text}</button>
+);
+
+const History = (props) => {
+	if (props.allClicks.length === 0) {
+		return <div>this app is used by pressing the buttons</div>;
+	}
+	return <div>button press history: {props.allClicks.join(' ')}</div>;
+};
 
 export default App;
