d67f4de3b03d0d5d9caa0a997a981d007f097893
Amandeep Singh <programmeramandeep@gmail.com>
2023-01-07 09:27:11 +0530

    Deploying app to the internet

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 let persons = [
 	{
@@ -27,6 +28,8 @@ let persons = [
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
