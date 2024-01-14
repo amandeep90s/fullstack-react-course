1cfb79f568d313fb4ee26ffbcec50220b2451e11
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-14 14:02:26 +0530

    Part 2 - Exercise 2.6 is completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
 const App = () => {
-	return <div>Phonebook App</div>;
+	const [persons, setPersons] = useState([{ name: "Amandeep Singh" }]);
+	const [newName, setNewName] = useState("");
+
+	const handleNewName = (e) => setNewName(e.target.value);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (newName) {
+			const newNameObject = { name: newName };
+
+			setPersons(persons.concat(newNameObject));
+			setNewName("");
+		}
+	};
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					name: <input type="text" value={newName} onChange={handleNewName} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+
+			<h2>Numbers</h2>
+			{persons.map((person, index) => (
+				<p key={person.name + index}>{person.name}</p>
+			))}
+		</div>
+	);
 };
 
 export default App;
