f9e8f38a04a6b7c39ead47504996bcaffa9e7285
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-15 16:39:56 +0530

    Part 2 - Inline styles

--- part2/notes-frontend/src/App.jsx
+++ part2/notes-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
 import noteService from "./services/notes";
@@ -78,6 +79,8 @@ const App = () => {
 
 				<button type="submit">save</button>
 			</form>
+
+			<Footer />
 		</div>
 	);
 };
