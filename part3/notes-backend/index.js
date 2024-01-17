97de83fa31841dae41c7e8fed7c757fe506506ec
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-17 21:33:27 +0530

    Part 3 - Fetching a single resource

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -28,5 +28,15 @@ app.get("/api/notes", (request, response) => {
 	response.json(notes);
 });
 
+app.get("/api/notes/:id", (request, response) => {
+	const id = Number(request.params.id);
+	const note = notes.find((n) => n.id === id);
+	if (note) {
+		response.json(note);
+	} else {
+		response.status(404).json({ message: "Note does not exist" });
+	}
+});
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
