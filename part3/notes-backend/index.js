c732ca11c1f91510494e91b29c48241b3fdc8ddb
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-17 21:07:21 +0530

    Part 3 - Added express and nodemon dependencies

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+const express = require("express");
 
 let notes = [
 	{
@@ -18,12 +18,15 @@ let notes = [
 	},
 ];
 
-const app = http.createServer((request, response) => {
-	response.writeHead(200, { "Content-Type": "application/json" });
-	response.end(JSON.stringify(notes));
+const app = express();
+
+app.get("/", (request, response) => {
+	response.send("<h1>Hello world</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+	response.json(notes);
 });
 
 const PORT = 3001;
-app.listen(PORT);
-
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
