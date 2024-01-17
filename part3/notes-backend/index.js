935233a2a62ded3636268d7a70240e3259044cbb
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-17 22:28:01 +0530

    Part 3 - Receiving and adding data

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -20,6 +20,8 @@ let notes = [
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
 	response.send("<h1>Hello world</h1>");
 });
@@ -36,6 +38,34 @@ app.get("/api/notes/:id", (request, response) => {
 	} else {
 		response.status(404).json({ message: "Note does not exist" });
 	}
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+	const id = Number(request.params.id);
+	notes = notes.filter((n) => n.id !== id);
+	response.status(204).end();
+});
+
+const generateId = () => {
+	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+	return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+	const { body } = request;
+
+	if (!body.content) {
+		return response.status(400).json({ error: "Content is missing" });
+	}
+
+	const note = {
+		content: body.content,
+		important: Boolean(body.important) || false,
+		id: generateId(),
+	};
+
+	notes = notes.concat(note);
+	response.json(note);
 });
 
 const PORT = 3001;
