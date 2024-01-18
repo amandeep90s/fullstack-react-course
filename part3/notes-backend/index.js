75ce25d1dab7e4c784cb84e49531f007bb921dd8
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-18 22:28:15 +0530

    Part 3 - The order of middleware loading

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -3,6 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const Note = require("./models/note");
 
+const requestLogger = (request, response, next) => {
+	console.log("Method:", request.method);
+	console.log("Path:", request.path);
+	console.log("Body:", request.body);
+	console.log("----");
+	next();
+};
+
 const app = express();
 
 app.use(cors());
@@ -11,29 +19,41 @@ app.use(express.json());
 
 app.use(express.static("dist"));
 
+app.use(requestLogger);
+
 app.get("/", (request, response) => {
 	response.send("<h1>Hello world</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-	Note.find({}).then((notes) => {
-		response.json(notes);
-	});
+app.get("/api/notes", (request, response, next) => {
+	Note.find({})
+		.then((notes) => {
+			response.json(notes);
+		})
+		.catch((error) => next(error));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-	Note.findById(request.params.id).then((note) => {
-		response.json(note);
-	});
+app.get("/api/notes/:id", (request, response, next) => {
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (note) {
+				response.json(note);
+			} else {
+				response.status(404).json({ error: "Note does not found" });
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-	Note.findByIdAndDelete(request.params.id).then((note) => {
-		response.status(204).end();
-	});
+app.delete("/api/notes/:id", (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then(() => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
 	const { body } = request;
 
 	if (!body.content) {
@@ -45,12 +65,15 @@ app.post("/api/notes", (request, response) => {
 		important: Boolean(body.important) || false,
 	});
 
-	note.save().then((savedNote) => {
-		response.json(savedNote);
-	});
+	note
+		.save()
+		.then((savedNote) => {
+			response.json(savedNote);
+		})
+		.catch((error) => next(error));
 });
 
-app.put("/api/notes/:id", (request, response) => {
+app.put("/api/notes/:id", (request, response, next) => {
 	const { body } = request;
 
 	if (!body.content) {
@@ -66,11 +89,28 @@ app.put("/api/notes/:id", (request, response) => {
 		.then((updatedNote) => {
 			response.json(updatedNote);
 		})
-		.catch((error) => {
-			console.error(error);
-			response.status(500).json({ error: "Internal Server Error" });
-		});
+		.catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === "CastError") {
+		return response.status(400).send({ error: "malformatted id" });
+	}
+
+	next(error);
+};
+
+// handler of requests with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
