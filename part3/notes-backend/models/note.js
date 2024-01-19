34730f01d9d52750b2ed165900996620c0cd9dde
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-19 22:30:31 +0530

    Part 3 - Exercise 3.22 is completed

--- part3/notes-backend/models/note.js
+++ part3/notes-backend/models/note.js
@@ -1,36 +1,36 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 
 const url = process.env.MONGODB_URI;
 
 mongoose
-	.connect(url)
-	.then((result) => {
-		console.log("connected to MongoDB");
-	})
-	.catch((error) => {
-		console.log("error connecting to MongoDB:", error.message);
-	});
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
-	content: {
-		type: String,
-		minLength: 5,
-		required: true,
-	},
-	important: {
-		type: Boolean,
-		default: false,
-	},
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-noteSchema.set("toJSON", {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema);
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,36 +1,36 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const url = process.env.MONGODB_URI;
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+  name: String,
+  number: String,
 });
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema);
 
 if (process.argv.length === 2) {
-	Person.find({}).then((result) => {
-		console.log("phonebook:");
-		result.forEach((person) => {
-			console.log(person.name, person.number);
-		});
-		mongoose.connection.close();
-	});
+  Person.find({}).then((result) => {
+    console.log('phonebook:');
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
 } else if (process.argv.length === 4) {
-	const person = new Person({
-		name: process.argv[2],
-		number: process.argv[3],
-	});
+  const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3],
+  });
 
-	person.save().then((result) => {
-		console.log(
-			`added ${process.argv[2]}, number ${process.argv[3]} to phonebook`
-		);
-		mongoose.connection.close();
-	});
+  person.save().then(() => {
+    console.log(
+      `added ${process.argv[2]}, number ${process.argv[3]} to phonebook`
+    );
+    mongoose.connection.close();
+  });
 }
