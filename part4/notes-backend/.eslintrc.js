dea9f059723511e850953ab9b9678a670022a41b
Amandeep Singh <programmeramandeep@gmail.com>
2024-01-20 11:34:52 +0530

    Part 4 - Project structure notes backend

--- part4/notes-backend/.eslintrc.js
+++ part4/notes-backend/.eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     eqeqeq: 'error',
--- part4/notes-backend/index.js
+++ part4/notes-backend/index.js
@@ -1,122 +1,38 @@
 require('dotenv').config();
-const express = require('express');
+const config = require('./utils/config');
 const cors = require('cors');
-const Note = require('./models/note');
+const express = require('express');
+const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
+const mongoose = require('mongoose');
+const notesRouter = require('./controllers/notes');
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:', request.path);
-  console.log('Body:', request.body);
-  console.log('----');
-  next();
-};
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.static('dist'));
+app.use(middleware.requestLogger);
 
-app.use(requestLogger);
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello world</h1>');
-});
-
-app.get('/api/notes', (request, response, next) => {
-  Note.find({})
-    .then((notes) => {
-      response.json(notes);
-    })
-    .catch((error) => next(error));
-});
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).json({ error: 'Note does not found' });
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-app.post('/api/notes', (request, response, next) => {
-  const { body } = request;
-
-  if (!body.content) {
-    return response.status(400).json({ error: 'Content is missing' });
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: Boolean(body.important) || false,
-  });
-
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
-});
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { body } = request;
-
-  if (!body.content) {
-    return response.status(400).json({ error: 'Content is missing' });
-  }
-
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, {
-    new: true,
-    runValidators: true,
-    context: 'query',
-  })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+app.use('/api/notes', notesRouter);
 
 // handler of requests with unknown endpoint
-app.use(unknownEndpoint);
-
-const errorHandler = (error, _request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
+app.use(middleware.unknownEndpoint);
 
 // handler of requests with result to errors
-app.use(errorHandler);
+app.use(middleware.errorHandler);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(config.PORT, () =>
+  logger.info(`Server running on port ${config.PORT}`)
+);
