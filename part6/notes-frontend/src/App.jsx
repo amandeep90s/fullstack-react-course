b7b66a6d75b36dc7fb1c8962f8ae689818ec15f3
Amandeep Singh <programmeramandeep@gmail.com>
2024-03-29 11:51:12 +0530

    Part 6 - More components

--- part6/notes-frontend/src/App.jsx
+++ part6/notes-frontend/src/App.jsx
@@ -1,46 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { addNote, toggleImportance } from './redux/features/notesSlice';
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+import AddNote from './components/AddNote';
+import Notes from './components/Notes';
 
 const App = () => {
-  const dispatch = useDispatch();
-  const { notes } = useSelector((state) => state.notes);
-
-  const addNewNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = '';
-
-    dispatch(
-      addNote({
-        content,
-        important: false,
-        id: generateId(),
-      })
-    );
-  };
-
-  const toggleNoteImportance = (id) => {
-    dispatch(toggleImportance({ id }));
-  };
-
   return (
     <div>
-      <form onSubmit={addNewNote}>
-        <input type='text' name='note' />
-        <button type='submit'>Add note</button>
-      </form>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.id}>
-            {note.content}
-            <button type='button' onClick={() => toggleNoteImportance(note.id)}>
-              <strong>{note.important ? 'Important' : 'Not Important'}</strong>
-            </button>
-          </li>
-        ))}
-      </ul>
+      <AddNote />
+      <Notes />
     </div>
   );
 };
---
+++ part6/notes-frontend/src/components/AddNote.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addNote } from '../redux/features/notesSlice';
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+const AddNote = () => {
+  const dispatch = useDispatch();
+
+  const addNewNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = '';
+
+    dispatch(
+      addNote({
+        content,
+        important: false,
+        id: generateId(),
+      })
+    );
+  };
+
+  return (
+    <form onSubmit={addNewNote}>
+      <input type='text' name='note' />
+      <button type='submit'>Add note</button>
+    </form>
+  );
+};
+
+export default AddNote;
