433cac1191141fc106258380affae3726cefe135
Amandeep Singh <programmeramandeep@gmail.com>
2024-03-29 19:23:40 +0530

    Part 6 - Setup redux toolkit

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
-  return <div>App</div>;
+  const anecdotes = useSelector((state) => state.anecdotes);
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    console.log('vote', id);
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
