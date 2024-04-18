9b1e50ab861d2c4bf04dc116f313a41686461dfb
Amandeep Singh <programmeramandeep@gmail.com>
2024-04-18 21:57:53 +0530

    Part 7  - Parameterized routes app setup

--- part7/router-app/src/App.jsx
+++ part7/router-app/src/App.jsx
@@ -1,50 +1,175 @@
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import {
+  Link,
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useNavigate,
+  useParams,
+} from 'react-router-dom';
 
 const Home = () => (
   <div>
-    <h2>Home Page</h2>
+    <h2>TKTL notes app</h2>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry&apos;s standard dummy text ever since
+      the 1500s, when an unknown printer took a galley of type and scrambled it
+      to make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </p>
   </div>
 );
 
-const Notes = () => (
+const Note = ({ notes }) => {
+  const id = useParams().id;
+  const note = notes.find((note) => note.id === Number(id));
+
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>{note.important ? 'important' : ''}</strong>
+      </div>
+    </div>
+  );
+};
+
+Note.propTypes = {
+  notes: PropTypes.array,
+};
+
+const Notes = ({ notes }) => (
   <div>
     <h2>Notes Page</h2>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
+
+Notes.propTypes = {
+  notes: PropTypes.array,
+};
 
 const Users = () => (
   <div>
-    <h2>Users Page</h2>
+    <h2>TKTL notes app</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
   </div>
 );
 
-const App = () => {
-  const padding = { padding: 5 };
+const Login = (props) => {
+  const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    props.onLogin('amandeep');
+    navigate('/');
+  };
 
   return (
-    <Router>
-      <div>
-        <Link style={padding} to='/'>
-          Home
-        </Link>
-        <Link style={padding} to='/notes'>
-          Notes
-        </Link>
-        <Link style={padding} to='/users'>
-          Users
-        </Link>
-      </div>
+    <div>
+      <h2>Login</h2>
 
-      <Routes>
-        <Route path='/notes' element={<Notes />} />
-        <Route path='/users' element={<Users />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input type='text' />
+        </div>
 
-      <div>
-        <i>Note app, Department of Computer Science 2024</i>
-      </div>
-    </Router>
+        <div>
+          password: <input type='password' />
+        </div>
+
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  );
+};
+
+Login.propTypes = {
+  onLogin: PropTypes.func,
+};
+
+const App = () => {
+  const padding = { padding: 5 };
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas',
+    },
+  ]);
+  const [user, setUser] = useState(null);
+
+  const login = (user) => setUser(user);
+
+  return (
+    <div>
+      <Router>
+        <div>
+          <Link style={padding} to='/'>
+            Home
+          </Link>
+          <Link style={padding} to='/notes'>
+            Notes
+          </Link>
+          <Link style={padding} to='/users'>
+            Users
+          </Link>
+          {user ? (
+            <em>{user} logged in</em>
+          ) : (
+            <Link style={padding} to='/login'>
+              Login
+            </Link>
+          )}
+        </div>
+
+        <Routes>
+          <Route path='/notes/:id' element={<Note notes={notes} />} />
+          <Route path='/notes' element={<Notes notes={notes} />} />
+          <Route
+            path='/users'
+            element={user ? <Users /> : <Navigate replace to='/login' />}
+          />
+          <Route path='/login' element={<Login onLogin={login} />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+
+        <div>
+          <i>Note app, Department of Computer Science 2024</i>
+        </div>
+      </Router>
+    </div>
   );
 };
 
